4c11b973bf07606c44c1a66fac928423b45f07c2
cafesao <gabriel.diasdutra2@gmail.com>
2020-04-27 19:04:23 -0300

    Adicionando modulo do Alert no Root

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const options = {
+  position: 'top center',
+  timeout: 2500,
+  offset: '8px',
+  transition: 'scale',
+}
+
+const Root = () => (
+  <AlertProvider template={AlertTemplate} {...options}>
+    <App />
+  </AlertProvider>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'))
